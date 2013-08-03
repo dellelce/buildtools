8b421fef5175bcfeb089e5cb257845ba21fb4241
Antonio Dell'Elce <adellelce@hotmail.com>
2013-08-03 19:47:35 +0200

    initial upload

---
+++ build.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#
+# File:         build_phc.sh
+# Created:      1623 020613
+# Description:  description for build_phc.sh
+#
+
+### FUNCTIONS ###
+
+now () 
+{ 
+    export now=$(date +%H%M_%d%m%y);
+    echo $now
+}
+
+fail()
+{
+  echo "$*"
+  exit 1
+}
+
+### ENV ###
+
+
+buildFile="$1"
+[ ! -f "$buildFile" ] &&
+{
+  prefix="/home/antonio/src/web/mediawiki/mwpython/root"
+  src="$PWD/bison-2.7.1"
+  build="build_2255_200613"
+  id="confbuild_bison"
+} ||
+{
+  . "${buildFile}"
+  [ -z "${prefix}" -o -z "${src}" -o -z "${build}" -o -z "${id}" ] && fail "invalid profile"
+}
+
+logsdir="$PWD/logs"
+logfile="${logsdir}/log.${id}.$(now).txt"
+
+
+### MAIN ###
+cat << EOF
+
+ID is       ${id}
+Source is   ${src}
+Build is    ${build}
+Log file is ${logfile}
+
+EOF
+
+{
+  set -x
+  export CFLAGS="-I$prefix/include"
+  export CXXFLAGS="-I$prefix/include"
+  export LDFLAGS="-L$prefix/lib"
+  
+  cd "$build"
+  
+#  [ -z "$configure_args" ] &&
+#  {
+#     $src/configure   \
+#		--prefix=$prefix      \
+#		2>&1
+#  } ||
+#  {
+#     $src/configure   \
+#		--prefix=$prefix      \
+#		${configure_args}
+#		2>&1
+#  }
+  
+  set +x
+  
+  rc=0
+  [ "$rc" -ne 0 ] && make  2>&1
+  
+  rc=$?
+  [ "$rc" -ne 0 ] && make  install 2>&1
+} 2>&1 |  tee "${logfile}"
+
+
+### EOF ###
